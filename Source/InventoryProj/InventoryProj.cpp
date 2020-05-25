e1df87f180592e0bcda4e45035d4ecb905ba05f4
Skierhou <675363508@qq.com>
2020-05-25 18:05:36 +0800

    1

---
+++ Source/InventoryProj/InventoryProj.cpp
@@ -0,0 +1,6 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#include "InventoryProj.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, InventoryProj, "InventoryProj" );
